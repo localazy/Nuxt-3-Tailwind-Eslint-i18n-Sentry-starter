78f82f4db75fd8c80771f2251bc0b698652e7922
Daniel Charvát <dan@localazy.com>
2022-07-27 10:28:15 +0200

    Add eslint.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  },
 })
